602ff2ed3b111269b2af1899a394f5689867ee6a
Renato Crudo <renato.almeida@soupilar.com.br>
2024-07-11 22:14:10 -0300

    add service `catedral`

---
+++ catedral/catedral/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+
+engine = create_async_engine('postgresql+psycopg://app_user:app_password@dbpilar:5432/app_db')
+
+async def get_session():
+    async with AsyncSession(engine) as session:
+        yield session
